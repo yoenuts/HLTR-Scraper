e43baceed045d188eef6126a90b0d1444e4e9fc5
Erlein Nicole <erleinureta@gmail.com>
2023-08-26 17:14:36 +0800

    downloads epub and saves to a specified directory

--- gutenberg/gutenbergScrape.js
+++ gutenberg/gutenbergScrape.js
@@ -1,9 +1,10 @@
 const cheerio = require('cheerio');
-const axios = require('axios')
+const axios = require('axios');
 const fs = require('fs');
-const { log } = require('console');
+const path = require('path');
+const os = require('os');
 
-const URL = 'https://gutenberg.org/ebooks/2701'
+const URL = 'https://gutenberg.org/ebooks/2701';
 
 //download complete books, get their word counts, and also get example passages from the books
 
@@ -20,45 +21,29 @@ async function downloadHTML(url, filePath){
 
 }
 
-async function queryBook(filePath, fileName){
-    const html = fs.readFileSync(filePath, 'utf-8');
-    const $ = cheerio.load(html);
-
-}
-
-/*
-async function queryBook(url){
+async function queryBook(filePath, epubfilePath){
     try{
-        response = await axios.get(url,{
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
-            }
+        const html = fs.readFileSync(filePath, 'utf-8');
+        const $ = cheerio.load(html);
     
-        });
-        const $ = cheerio.load(response.data);
-
-        //
+    
         const title = await getTitle($);
         const epubLink = await getDownloadLink($);
-        if(epubLink){
-            
-        }
-
+        await downloadLink(title, epubLink, epubfilePath);
+        const passage = await getPassage();
         const bookInfo = {
             title,
             epubLink
         }
 
         console.log(JSON.stringify(bookInfo, null, 2));
-
-    } catch(error){
-        console.log("Error:", error);
-    }
     
-
-
+    }
+    catch(error){
+        throw 'error reading html file';
+    }
 }
-*/
+
 
 async function getTitle($){
     const titleElement = $('h1');
@@ -77,12 +62,60 @@ async function getDownloadLink($){
 }
 
 
-function downloadLink(url){
+async function downloadLink(title, url, filePath){
+    const epubName = title + '.epub';
+    const output = path.join(filePath, epubName);
+    try{
+
+        const response = await axios({
+            method: 'get',
+            url: url,
+            responseType: 'stream'
+        });
+
+        const file = fs.createWriteStream(output);
+        response.data.pipe(file);
+
+        return new Promise((resolve, reject) => {
+            file.on('finish', resolve);
+            file.on('error', reject);
+        })
+    }
+    catch(error){
+        console.log('Error:',error);
+    }
+}
+
+async function getPassage(){
+
+}
+
+async function getWordCount(){
     
 }
 
-async function getPassages(){
 
+//file name
+const htmlFileName = URL.split('/ebooks/')[1] + '.html';
+const epubFileName = URL.split('/ebooks/')[1] + '.epub';
+//file path. Folder to store html files was in my computer's Documents
+const documentsPath = path.join(os.homedir(), 'Documents');
+const htmlfolderPath = path.join(documentsPath, 'gutenberg-book-html');
+//store epubs in a filder within gutenberg-book-html
+const epubfolderPath = path.join(htmlfolderPath, 'gutenberg-book-epubs');
+
+//check if gutenberg-book-html and gutenberg-book-epubs folder exist in the computer directory. create one if it doesnt
+if (!fs.existsSync(htmlfolderPath)) {
+    // Create the folder if it doesn't exist
+    fs.mkdirSync(htmlfolderPath);
+    console.log('Folder created:', htmlfolderPath);
 }
+if(!fs.existsSync(epubfolderPath)){
+    fs.mkdirSync(epubfolderPath);
+    console.log('Folder created:', epubfolderPath);
+};
 
-queryBook(URL);
+const htmlFilePath = path.join(htmlfolderPath, htmlFileName);
+downloadHTML(URL, htmlFilePath).then(() => {
+    queryBook(htmlFilePath, epubfolderPath);
+});
